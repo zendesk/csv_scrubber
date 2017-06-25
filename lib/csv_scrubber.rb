a8d6c7f1ae66f0f04b25263e00ba0b57ac21126a
Miguel Tineo <miketineo@gmail.com>
2017-06-25 10:36:54 -0500

    Add specs

--- lib/csv_scrubber.rb
+++ lib/csv_scrubber.rb
@@ -1,2 +1,14 @@
-module CsvScrubber
+module Zendesk
+  module CsvScrubber
+    def self.scrub_output(value)
+      # escape macro operators at the beginning of strings to
+      # prevent malicious macro injection, e.g. (=, +, -, @)
+      value = "'#{value}" if macro_string?(value)
+      value
+    end
+
+    def self.macro_string?(value)
+      value.is_a?(String) && value.size > 1 && value.start_with?("=", "+", "-", "@")
+    end
+  end
 end
--- spec/csv_scrubber_spec.rb
+++ spec/csv_scrubber_spec.rb
@@ -6,4 +6,8 @@ describe CsvScrubber do
   it "has a VERSION" do
     expect(CsvScrubber::VERSION).to match(/^[\.\da-z]+$/)
   end
+
+  it "escapes macro strings" do
+    expect(Zendesk::CsvScrubber.scrub_output('=1+1')).to eq("'=1+1")
+  end
 end
